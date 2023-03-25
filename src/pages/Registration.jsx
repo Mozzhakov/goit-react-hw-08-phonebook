024710b75a3465a88be07cc50523b695b8e83d9f
Mozzhakov <imozzhakov@icloud.com>
2023-03-26 01:36:32 +0200

    upd validation

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -43,12 +43,7 @@ export default function Registration() {
             <Typography component="h1" variant="h5">
               Sign up
             </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
-            >
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
                 margin="normal"
                 required
